3d6781fda49d4ef3bc9720e5bca60054d562e213
slspeidel <slspeidel@comcast.net>
2016-11-14 16:36:04 -0700

    items per page buttons

--- pokeDashboard/src/App.js
+++ pokeDashboard/src/App.js
@@ -3,27 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 import PokeList from './components/PokeList';
-import {Col} from 'react-bootstrap/lib/';
+import {Col, Pagination} from 'react-bootstrap/lib/';
+import SelectItemsPerPageButtons from './components/SelectItemsPerPageButtons';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        pokemon: []
+        pokemon: [],
+        activePage: 0,
+        limit: 50,
+        offset: 0,
+        totalPages: 0
     };
 
     this.loadPokemon = this.loadPokemon.bind(this);
+    this.handlePaginationSelect = this.handlePaginationSelect.bind(this);
+    this.handleLimitChange = this.handleLimitChange.bind(this);
   }
 
   loadPokemon(url) {
+    console.log(url);
     fetch(url)
       .then(response => {
         return response.json();
       }).then(json => {
         console.log(json);
+        let pages = Math.round(json.count / this.state.limit);
         this.setState({
-          pokemon: json.results
+          pokemon: json.results,
+          totalPages: pages,
+          count: json.count
         });
         console.log(this.state);
       }).catch(err => {
@@ -32,7 +43,22 @@ class App extends Component {
   }
 
   componentWillMount() {
-    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
+    console.log(`${this.props.baseUrl}/pokemon/?limit`);
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${this.state.offset}`);
+  }
+
+  handlePaginationSelect(selectedPage) {
+    console.log(selectedPage);
+    let offset = this.state.limit * (selectedPage - 1);
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${offset}`);
+  }
+
+  handleLimitChange(event) {
+    this.setState({
+      limit: +event.target.innerHTML || this.state.count
+    }, () => {
+      this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=0`)
+    })
   }
   
   render() {
@@ -42,8 +68,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Pokemon Creatures</h2>
         </div>
+        <SelectItemsPerPageButtons options={[10, 50, 100, 200]} selectedValue={this.state.limit} allValue={this.state.count} onOptionSelected={this.handleLimitChange} />
         <Col sm={8} md={10} smOffset={2} mdOffset={1} >
           <PokeList listOfPokemon={this.state.pokemon} />
+        </Col>
+
+        <Col sm={12}>
+          <Pagination 
+            bsSize="small" 
+            items={this.state.totalPages}
+            activePage={this.state.activePage} 
+            onSelect={this.handlePaginationSelect} 
+          />
         </Col>
       </div>
     );
